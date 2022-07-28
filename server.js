8446e831c2058b22e0b5df3f909d3d6392ae9bba
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-07-28 14:06:33 -0400

    added switch statement and seed

--- server.js
+++ server.js
@@ -13,29 +13,77 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-  });
+});
 
-  //Inquirer Prompt 
-  const promptUser = () => {
+//Inquirer Prompt 
+const promptUser = () => {
     inquirer.prompt([
         {
             type: 'list',
             name: 'choices',
             message: 'What would you like to do?',
-            choices: ['View all departments', 
-            'View all roles', 
-            'View all employees', 
-            'Add a department', 
-            'Add a role', 
-            'Add an employee', 
-            'Update an employee role',
-            'Update an employee manager',
-            "View employees by department",
-            'Delete a department',
-            'Delete a role',
-            'Delete an employee',
-            'View department budgets',
-            'No Action']
+            choices: ['View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Update an employee manager',
+                "View employees by department",
+                'Delete a department',
+                'Delete a role',
+                'Delete an employee',
+                'View department budgets',
+                'No Action']
         }
     ])
-  }
+        .then((answers) => {
+            const { choices } = answers;
+
+            switch (answers) {
+                case 'View all Departments':
+                    showDepartments();
+                    break;
+                case 'View all roles':
+                    showRoles();
+                    break;
+                case 'View all employees':
+                    showEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployee();
+                    break;
+                case 'Update an employee manager':
+                    updateManager();
+                    break;
+                case 'View employees by department':
+                    employeeDepartment();
+                    break;
+                case 'Delete a department':
+                    deleteDepartment();
+                    break;
+                case 'Delete a role':
+                    deleteRole();
+                    break;
+                case 'Delete an employee':
+                    deleteEmployee();
+                    break;
+                case 'View department budgets':
+                    viewBudget();
+                    break;
+                case 'No Action':
+                    connection.end()
+                    break;
+            }
+        })
+}
