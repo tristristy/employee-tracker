2af9198a93239a029a56300d8a7f30b064f5307a
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-07-28 08:57:08 -0400

    added inquirer choice prompt

--- server.js
+++ server.js
@@ -7,3 +7,35 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 require('dotenv').config()
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+
+  //Inquirer Prompt 
+  const promptUser = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 
+            'View all roles', 
+            'View all employees', 
+            'Add a department', 
+            'Add a role', 
+            'Add an employee', 
+            'Update an employee role',
+            'Update an employee manager',
+            "View employees by department",
+            'Delete a department',
+            'Delete a role',
+            'Delete an employee',
+            'View department budgets',
+            'No Action']
+        }
+    ])
+  }
