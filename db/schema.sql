53349a4c5690fe2be747578a3a1a71015f8884ff
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-07-29 13:57:27 -0400

    fixed inquirer, got first three functions working

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS employee_db;
-CREATE DATABSE employee_db;
+CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
@@ -11,9 +11,9 @@ CREATE TABLE role (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL, 
-    department_id INTEGER,
+    department_id INTEGER NOT NULL,
     INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -22,7 +22,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INTEGER,
     INDEX manager_ind (manager_id),
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
--- server.js
+++ server.js
@@ -1,130 +1,137 @@
 //Imports
-
 const mysql = require('mysql2');
-
 const inquirer = require('inquirer');
-
 const cTable = require('console.table');
-
 require('dotenv').config()
 
+//Create Connection instance variable
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-connection.connect((err) =>{
-    if (err) throw err;
-    console.log('connected as id' + connection.threadId)
-    console.log();
-    promptUser();
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('connected as id' + connection.threadId)
+  promptUser();
 })
 
 //Inquirer Prompt 
 const promptUser = () => {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'choices',
-            message: 'What would you like to do?',
-            choices: ['View all departments',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role',
-                'Update an employee manager',
-                "View employees by department",
-                'Delete a department',
-                'Delete a role',
-                'Delete an employee',
-                'View department budgets',
-                'No Action']
-        }
-    ])
-        .then((answers) => {
-            const { choices } = answers;
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choices',
+      message: 'What would you like to do?',
+      choices: ['View all Departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Update an employee manager',
+        "View employees by department",
+        'Delete a department',
+        'Delete a role',
+        'Delete an employee',
+        'View department budgets',
+        'No Action']
+    }
+  ])
+    .then((answers) => {
+      // const { choices } = answers;
 
-            switch (answers) {
-                case 'View all Departments':
-                    showDepartments();
-                    break;
-                case 'View all roles':
-                    showRoles();
-                    break;
-                case 'View all employees':
-                    showEmployees();
-                    break;
-                case 'Add a department':
-                    addDepartment();
-                    break;
-                case 'Add a role':
-                    addRole();
-                    break;
-                case 'Add an employee':
-                    addEmployee();
-                    break;
-                case 'Update an employee role':
-                    updateEmployee();
-                    break;
-                case 'Update an employee manager':
-                    updateManager();
-                    break;
-                case 'View employees by department':
-                    employeeDepartment();
-                    break;
-                case 'Delete a department':
-                    deleteDepartment();
-                    break;
-                case 'Delete a role':
-                    deleteRole();
-                    break;
-                case 'Delete an employee':
-                    deleteEmployee();
-                    break;
-                case 'View department budgets':
-                    viewBudget();
-                    break;
-                case 'No Action':
-                    connection.end()
-                    break;
-            }
-        })
+      //massive switch statement because if-> else chain would be too much.
+      switch (answers.choices) {
+          case 'View all Departments':
+              showDepartments();
+              break;
+          case 'View all roles':
+              showRoles();
+              break;
+          case 'View all employees':
+              showEmployees();
+              break;
+          case 'Add a department':
+              addDepartment();
+              break;
+          case 'Add a role':
+              addRole();
+              break;
+          case 'Add an employee':
+              addEmployee();
+              break;
+          case 'Update an employee role':
+              updateEmployee();
+              break;
+          case 'Update an employee manager':
+              updateManager();
+              break;
+          case 'View employees by department':
+              employeeDepartment();
+              break;
+          case 'Delete a department':
+              deleteDepartment();
+              break;
+          case 'Delete a role':
+              deleteRole();
+              break;
+          case 'Delete an employee':
+              deleteEmployee();
+              break;
+          case 'View department budgets':
+              viewBudget();
+              break;
+          case 'No Action':
+              connection.end();
+              break;
+      }
+      
+    })
 };
 
 //Function that pulls all departments and logs them in a table, in the console.
 
 showDepartments = () => {
-    console.log('Showing all departments...');
-    const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
-  
-    connection.promise().query(sql, (err, rows) => {
-      if (err) throw err;
-      console.table(rows);
-      promptUser();
-    });
-  };
-  
+  console.log('Showing all departments...');
+  const sql = `SELECT department.id AS id, department.name AS department FROM department`;
 
-  showRoles = () => {
-    console.log('Showing all roles...');
-  
-    const sql = `SELECT role.id, role.title, department.name AS department
+  connection.query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  });
+};
+
+
+showRoles = () => {
+  console.log('Showing all roles...');
+
+  const sql = `SELECT role.id, role.title, department.name AS department
                  FROM role
                  INNER JOIN department ON role.department_id = department.id`;
-    
-    connection.promise().query(sql, (err, rows) => {
-      if (err) throw err; 
-      console.table(rows); 
-      promptUser();
-    })
-  };
+
+  connection.query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  })
+};
 
 showEmployees = () => {
+  console.log('Showing all Employees...');
 
+  const sql = `SELECT employee.id, employee.first_name, employee.last_name, manager_id
+              FROM employee`
+
+  connection.query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  })
 };
 
 addDepartment = () => {
@@ -142,3 +149,7 @@ addEmployee = () => {
 updateEmployee = () => {
 
 };
+
+employeeDepartment = () => {
+  
+}
